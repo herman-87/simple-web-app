6291d63a623e8564777a18f52b9935d7b27a64f7
herman87 <hermanessoungou@gmail.com>
2023-04-04 09:51:10 +0100

    I arange hierachie of data test

--- src/test/java/com/herman87/simplewebapp/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/herman87/simplewebapp/repository/DepartmentRepositoryTest.java
@@ -1,54 +1,87 @@
 package com.herman87.simplewebapp.repository;
 
 import com.herman87.simplewebapp.domain.Department;
+import com.herman87.simplewebapp.repository.DepartmentRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-class DepartmentRepositoryTest {
+public class DepartmentRepositoryTest {
     @Autowired
-    private DepartmentRepository departmentRepository;
+    TestEntityManager testEntityManager;
+    String name = "name";
+    String code = "code";
+    String address = "address";
     @Autowired
-    private TestEntityManager testEntityManager;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private DepartmentRepository objectUnderTest;
 
     @Test
-    @DisplayName("Test find by Id method")
-    void findByIdTest() {
-        final var department = testEntityManager.persist(
-                Department.builder()
-                        .code("department code")
-                        .name("department name")
-                        .address("department address")
-                        .build()
-        );
-
-        assertThat(departmentRepository.findById(department.getId()).orElseThrow())
-                .isEqualTo(department);
+    @DisplayName("test get by id method")
+    void crudTest() {
+        Long departmentId = create();
+        findById(departmentId);
+        findAll(departmentId);
+        update(departmentId);
+        delete(departmentId);
     }
 
-    @Test
-    @DisplayName("Test save Department")
-    void saveDepartmentTest() {
-        final var department = Department.builder()
-                .code("department code2")
-                .name("department name")
-                .address("department address")
+    void delete(Long id) {
+        objectUnderTest.deleteById(id);
+        assertThat(objectUnderTest.findAll())
+                .map(Department::getId)
+                .doesNotContain(id);
+    }
+
+    void update(Long id) {
+        Department department = objectUnderTest.findById(id).orElseThrow();
+        String newCode = "new code";
+        String newName = "new name";
+        String newAddress = "new address";
+        department.setCode(newCode);
+        department.setName(newName);
+        department.setAddress(newAddress);
+
+        assertThat(objectUnderTest.save(department))
+                .returns(newName, Department::getName)
+                .returns(newCode, Department::getCode)
+                .returns(newAddress, Department::getAddress);
+    }
+
+    void findAll(Long id) {
+        Department department = testEntityManager.persist(
+                Department.builder().name("name2").build());
+
+        assertThat(objectUnderTest.findAll())
+                .hasSize(2)
+                .map(Department::getId)
+                .contains(id, department.getId());
+    }
+
+    Department findById(Long id) {
+        Department department = objectUnderTest.findById(id).orElseThrow();
+        assertThat(department)
+                .returns(name, Department::getName)
+                .returns(code, Department::getCode)
+                .returns(address, Department::getAddress);
+
+        return department;
+    }
+
+    Long create() {
+        Department department = Department.builder()
+                .name(name)
+                .code(code)
+                .address(address)
                 .build();
-        departmentRepository.save(department);
 
-        assertThat(jdbcTemplate.queryForList("SELECT * FROM t_department"))
-                .map(stringObjectMap -> stringObjectMap.get("c_id"))
-                .map(Object::toString)
-                .map(Long::parseLong)
-                .contains(department.getId());
+        Department savedDepartment = objectUnderTest.save(department);
+        Long departmentId = savedDepartment.getId();
+        assertThat(departmentId).isNotNull();
+        return departmentId;
     }
-
 }
